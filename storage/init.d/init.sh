29ec410a86831ec1f54595add288f8232d79c4e9
kazu69 <kazu69web@gmail.com>
2016-01-23 11:50:07 +0900

    remove unuse files

--- storage/init.d/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mysql=( mysql --protocol=socket -uroot -p"${MYSQL_ROOT_PASSWORD}" )
-
-"${mysql[@]}" <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS '${MYSQL_DATABASE}';
-    GRANT ALL ON '${MYSQL_DATABASE}'.* TO '${MYSQL_USER}'@'%' ;
-EOSQL
--- storage/init.d/insert_mysql.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `personal` (`id`, `name`)
-SELECT * FROM (SELECT 1, 'root@local') AS tmp
-WHERE NOT EXISTS (
-    SELECT `id` FROM `personal` WHERE `id` = 1
-) LIMIT 1;
